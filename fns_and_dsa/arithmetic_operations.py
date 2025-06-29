b0912b125be0bf77740383a675c6ab264acb02c0
Boamah Evans Koranteng <legaltechrescue@gmail.com>
2025-06-29 12:20:08 +0000

    Add perform_operation function to handle basic arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+# arthmetic operations.py
+
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Cannot divide by zero"
+    else:
+        return "Erroe: Invalid operation"
